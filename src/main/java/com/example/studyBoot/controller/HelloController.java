c48f1dc2a1bbe93fc50870eb6f08a321f052e966
wjdgus43993 <71696834+wjdgus43993@users.noreply.github.com>
2022-02-18 17:45:26 +0900

    빌드하기,정적,mvc,api

--- src/main/java/com/example/studyBoot/controller/HelloController.java
+++ src/main/java/com/example/studyBoot/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.studyBoot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // Controller 추가
 public class HelloController {
@@ -14,4 +16,43 @@ public class HelloController {
         return "hello"; //templates 에서 'viewResolver'가 hello파일을 찾아서 실행시킴
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(
+            //RequestParam을 통해 받은 값을 view페이지에 뿌려준다
+            @RequestParam(value = "name",required = true)String name
+            ,Model m){
+
+        m.addAttribute("name",name);
+
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //그냥 바로 문자가 body에 입력되서 출력된다
+    public String helloString(@RequestParam("name")String name){
+        return "hello" + name;//"hello ~~ "
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //@ResponseBody로 한다면 json으로 반환하는게 기본.
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);//파라미터로 넘어온 name 을 입력
+        return hello; //문자가 아닌 객체를 넘김
+        //json 방식으로 출력됨
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
