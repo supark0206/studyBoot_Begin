aa536c50db29f79ea73ca971b648972e072431a2
wjdgus43993 <71696834+wjdgus43993@users.noreply.github.com>
2022-02-21 23:49:00 +0900

    회원 도메인 저장소 ,테스트까지 완료

---
+++ src/main/java/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package repository;
+
+import domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long,Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //null이 반활될 가능성이있을때 Optional사용
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                //파라미터 name과 같을때만 필터링됨
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //하나라도 찾으면 반환 끝까지없으면 null
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    
+    
+    //store를 비우는 메서드
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ src/test/java/com/example/studyBoot/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.example.studyBoot.repository;
+
+import domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import repository.MemberRepository;
+import repository.MemoryMemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    //중요!!  하나의 테스트가 끝날때마다 저장소나 공용 데이터를 비워줘야한다
+    //각 Test가 끝나고 데이터를 비워줌
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("test1");
+        
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+
+        System.out.println("result = " + (result==member));
+        //member가 result와 같은지 확인
+        //alt+enter로 스태틱 임포트 가능하다 그럼 더편하게 사용가능
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("findName1");
+        repository.save(member1);
+
+        //shift + f6으로 편하게 rename 가능하다
+        Member member2 = new Member();
+        member2.setName("findName2");
+        repository.save(member2);
+
+        Member result = repository.findByName("findName1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("findName1");
+        repository.save(member1);
+
+        //shift + f6으로 편하게 rename 가능하다
+        Member member2 = new Member();
+        member2.setName("findName2");
+        repository.save(member2);
+
+        List<Member> all = repository.findAll();
+
+        assertThat(all.size()).isEqualTo(2);
+
+
+    }
+
+}
